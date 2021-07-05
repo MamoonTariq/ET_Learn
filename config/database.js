6eb42896f84f949bb1b6d6c0a8bb3de8374a0db5
MamoonTariq <m.mamoon.tariq@gmail.com>
2021-07-05 12:34:28 -0700

    DB Connection

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-module.exports = function () {
+module.exports = () => {
   mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
   mongoose.connection.on("connected", () => {
     console.log("Mongoose Connected");
